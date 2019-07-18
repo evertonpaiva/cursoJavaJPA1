d8885f74212f73c05d0666b6619ea19fbcbc3ba5
Everton Paiva <evertonpaiva@gmail.com>
2019-07-18 09:12:39 -0300

    Revisão inicial do projeto. Configurando a Stack

---
+++ README.md
@@ -0,0 +1,5 @@
+# Java e JPA, parte 1
+
+## Persista seus objetos com a JPA2 e Hibernate
+
+Repositório do curso de Java e JPA parte 1, oferecido pela Alura.
---
+++ financas/src/br/com/caelum/financas/teste/TesteConta.java
@@ -0,0 +1,11 @@
+package br.com.caelum.financas.teste;
+
+import br.com.caelum.financas.modelo.Conta;
+
+public class TesteConta {
+
+	public static void main(String[] args) {
+		
+		Conta conta = new Conta();
+	}
+}
