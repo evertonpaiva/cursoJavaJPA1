90ab028f697e7439d65da3f528a84bd2a2924b36
Everton Paiva <evertonpaiva@gmail.com>
2019-07-18 09:44:05 -0300

    Criando uma classe utilitária para abstrair o uso do createEntityManagerFactory. Populando alguns dados para testes.

--- financas/src/br/com/caelum/financas/teste/TesteConta.java
+++ financas/src/br/com/caelum/financas/teste/TesteConta.java
@@ -5,6 +5,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.util.JPAUtil;
 
 public class TesteConta {
 
@@ -16,14 +17,12 @@ public class TesteConta {
 		conta.setBanco("Caixa Econômica");
 		conta.setNumero("456");
 		
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("financas");
-		EntityManager em = emf.createEntityManager();
+		EntityManager em = new JPAUtil().getEntityManager();
 		
 		em.getTransaction().begin();
 		em.persist(conta);
 		em.getTransaction().commit();
 		
 		em.close();
-		emf.close();
 	}
 }
