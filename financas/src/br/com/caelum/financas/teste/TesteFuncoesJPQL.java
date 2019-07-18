c004b37d0933ed96c4affd3db1f4d7284d70941d
Everton Paiva <evertonpaiva@gmail.com>
2019-07-18 14:33:07 -0300

    Criando o MovimentacaoDao

--- financas/src/br/com/caelum/financas/teste/TesteFuncoesJPQL.java
+++ financas/src/br/com/caelum/financas/teste/TesteFuncoesJPQL.java
@@ -3,8 +3,8 @@ package br.com.caelum.financas.teste;
 import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
 
+import br.com.caelum.financas.dao.MovimentacaoDao;
 import br.com.caelum.financas.modelo.Conta;
 import br.com.caelum.financas.modelo.TipoMovimentacao;
 import br.com.caelum.financas.util.JPAUtil;
@@ -19,21 +19,12 @@ public class TesteFuncoesJPQL {
 		Conta conta = new Conta();
 		conta.setId(2);
 
-		String jqpl = "select avg(m.valor) from Movimentacao m where m.conta = :pConta" +
-		" and m.tipo = :pTipo" +
-		" group by day(m.data), month(m.data), year(m.data)";
-		
-		TypedQuery<Double> query = em.createQuery(jqpl, Double.class);
-		query.setParameter("pConta", conta);
-		query.setParameter("pTipo", TipoMovimentacao.SAIDA);
-
-		List<Double> medias = (List<Double>) query.getResultList();
+		MovimentacaoDao dao = new MovimentacaoDao(em);
+		List<Double> medias = dao.getMediasPorDiaETipo(TipoMovimentacao.SAIDA, conta);
 				
 		for (Double media : medias) {
 			System.out.println("A média é: " + media);	
-		}
-			
-				
+		}		
 		
 		em.getTransaction().commit();
 		em.close();
