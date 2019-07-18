76284427290f17cee277a371aaf38d8e01d15515
Everton Paiva <evertonpaiva@gmail.com>
2019-07-18 10:39:44 -0300

    Mapeando relacionamentos entre entidades

---
+++ financas/src/br/com/caelum/financas/teste/TesteJPARelacionamento.java
@@ -0,0 +1,46 @@
+package br.com.caelum.financas.teste;
+
+import java.math.BigDecimal;
+import java.util.Calendar;
+
+import javax.persistence.EntityManager;
+
+import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.modelo.Movimentacao;
+import br.com.caelum.financas.modelo.TipoMovimentacao;
+import br.com.caelum.financas.util.JPAUtil;
+
+public class TesteJPARelacionamento {
+
+	public static void main(String[] args) {
+
+		Conta conta = new Conta();
+		conta.setAgencia("0109");
+		conta.setBanco("Itau");
+		conta.setNumero("1234");
+		conta.setTitular("Leonardo");
+		
+		Movimentacao movimentacao = new Movimentacao();
+		movimentacao.setData(Calendar.getInstance());
+		movimentacao.setDescricao("Churrascaria");
+		movimentacao.setTipo(TipoMovimentacao.ENTRADA);
+		movimentacao.setValor(new BigDecimal("200.0"));
+		
+		movimentacao.setConta(conta);
+		
+		
+		EntityManager em = new JPAUtil().getEntityManager();		
+		em.getTransaction().begin();
+		
+		em.persist(conta);
+		em.persist(movimentacao);
+		
+		em.getTransaction().commit();
+		em.close();
+		
+		
+		
+
+	}
+
+}
